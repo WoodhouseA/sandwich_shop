73be62da77ebc313e66825cac03e46ea65249548
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-12-03 13:40:44 +0000

    Refactor Cart class to extend ChangeNotifier and implement item removal, replacement, and quantity update features

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'sandwich.dart';
 import 'package:sandwich_shop/repositories/pricing_repository.dart';
 
-class Cart {
+class Cart extends ChangeNotifier {
   final Map<Sandwich, int> _items = {};
 
   // Returns a read-only copy of the items and their quantities
@@ -13,6 +14,7 @@ class Cart {
     } else {
       _items[sandwich] = quantity;
     }
+    notifyListeners();
   }
 
   void remove(Sandwich sandwich, {int quantity = 1}) {
@@ -23,11 +25,48 @@ class Cart {
       } else {
         _items.remove(sandwich);
       }
+      notifyListeners();
     }
   }
 
   void clear() {
     _items.clear();
+    notifyListeners();
+  }
+
+  void updateItemQuantity(Sandwich sandwich, int newQuantity) {
+    if (newQuantity <= 0) {
+      removeItem(sandwich);
+    } else {
+      _items[sandwich] = newQuantity;
+      notifyListeners();
+    }
+  }
+
+  void removeItem(Sandwich sandwich) {
+    if (_items.containsKey(sandwich)) {
+      _items.remove(sandwich);
+      notifyListeners();
+    }
+  }
+
+  void replaceItem(Sandwich oldSandwich, Sandwich newSandwich) {
+    if (_items.containsKey(oldSandwich)) {
+      final int quantity = _items[oldSandwich]!;
+      _items.remove(oldSandwich);
+      
+      // If the new sandwich already exists, merge quantities
+      if (_items.containsKey(newSandwich)) {
+        _items[newSandwich] = _items[newSandwich]! + quantity;
+      } else {
+        _items[newSandwich] = quantity;
+      }
+      notifyListeners();
+    }
+  }
+
+  void restoreRemovedItem(Sandwich sandwich, int quantity) {
+    add(sandwich, quantity: quantity);
   }
 
   double get totalPrice {
