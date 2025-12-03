566bf6968e33973661252622c66d078f2c6b7f4c
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-12-03 15:58:36 +0000

    Refactor App to use StatefulWidget and integrate Cart model for order management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/auth_screen.dart';
+import 'package:sandwich_shop/views/cart_screen.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
 import 'package:sandwich_shop/views/about_screen.dart';
 
@@ -7,16 +9,24 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Cart _cart = Cart();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
-      home: const OrderScreen(maxQuantity: 5),
+      home: OrderScreen(cart: _cart, maxQuantity: 5),
       routes: {
-        '/order': (context) => const OrderScreen(maxQuantity: 5),
+        '/order': (context) => OrderScreen(cart: _cart, maxQuantity: 5),
+        '/cart': (context) => CartScreen(cart: _cart),
         '/about': (context) => const AboutScreen(),
         '/auth': (context) => const AuthScreen(),
       },
