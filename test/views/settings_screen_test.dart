1b2ba4475d89a123a4e216578afd36d0c5674c95
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-12-06 16:45:16 +0000

    Add test for slider functionality in SettingsScreen to verify font size updates

--- test/views/settings_screen_test.dart
+++ test/views/settings_screen_test.dart
@@ -30,5 +30,19 @@ void main() {
       expect(find.text('This is sample text to preview the font size.'), findsOneWidget);
       expect(find.text('Back to Order'), findsOneWidget);
     });
+
+    testWidgets('slider updates font size', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: SettingsScreen()));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Current size: 16px'), findsOneWidget);
+      expect(AppStyles.baseFontSize, 16.0);
+
+      final Finder sliderFinder = find.byType(Slider);
+      await tester.drag(sliderFinder, const Offset(50, 0));
+      await tester.pumpAndSettle();
+      
+      expect(AppStyles.baseFontSize, isNot(16.0));
+    });
   });
 }
